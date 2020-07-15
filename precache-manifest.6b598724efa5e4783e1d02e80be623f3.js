a807e05b3b4d732f8c24caf054133ad5db89defe
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-07-15 14:21:43 -0700

    Updates

---
+++ precache-manifest.6b598724efa5e4783e1d02e80be623f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea807b731737cf925c7ada8ead316c35",
+    "url": "/kastjesfabriek/index.html"
+  },
+  {
+    "revision": "73a30658e7a74019627f",
+    "url": "/kastjesfabriek/static/css/main.bc57d5ef.chunk.css"
+  },
+  {
+    "revision": "b0a1a41ef58cdc3849bb",
+    "url": "/kastjesfabriek/static/js/2.57372f3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kastjesfabriek/static/js/2.57372f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a30658e7a74019627f",
+    "url": "/kastjesfabriek/static/js/main.b87a4dc4.chunk.js"
+  },
+  {
+    "revision": "a5de98025943a408367e",
+    "url": "/kastjesfabriek/static/js/runtime-main.f40a0b4d.js"
+  }
+]);
